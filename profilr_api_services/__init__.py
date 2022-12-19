2031ca00ad4b11c8efea60e3497d4b736337db16
Maurice Guikema <mguikema@gmail.com>
2022-12-19 14:07:10 +0100

    defaults promoted

--- profilr_api_services/__init__.py
+++ profilr_api_services/__init__.py
@@ -1,7 +1,8 @@
-from .msb import msb_api_service
+from .msb import msb_api_service, DEFAULT_FILTERS as MSB_DEFAULT_FILTERS
 from .profilr_api import profilr_api_service
 
 __all__ = [
     "msb_api_service",
     "profilr_api_service",
+    "MSB_DEFAULT_FILTERS",
 ]
