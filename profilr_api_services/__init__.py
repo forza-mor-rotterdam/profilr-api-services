d7b5db65d82a462bc497b612995499283506a4b6
Maurice Guikema <mguikema@gmail.com>
2023-01-16 15:52:43 +0100

    export api classes only

--- profilr_api_services/__init__.py
+++ profilr_api_services/__init__.py
@@ -1,2 +1,2 @@
 from .profilr_api import ProfilrApi as ProfileAPIService
-# from .msb import MSBService as IncidentAPIService , DEFAULT_FILTERS as MSB_DEFAULT_FILTERS, VALID_FILTERS as MSB_VALID_FILTERS
+from .msb import MSBService# as IncidentAPIService , DEFAULT_FILTERS as MSB_DEFAULT_FILTERS, VALID_FILTERS as MSB_VALID_FILTERS
