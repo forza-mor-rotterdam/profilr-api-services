5752e62a58c0d4e69e13724b7df62516ec1babc5
Remco Hoff <remco@code010.nl>
2023-01-24 15:39:43 +0100

    Update default_incident_api.py

--- profilr_api_services/default_incident_api.py
+++ profilr_api_services/default_incident_api.py
@@ -12,150 +12,170 @@ class DefaultIncidentAPIService(APIService):
         raise NotImplementedError("You must implement logout")
 
     def login(self):
+        """
+        post login {
+            username: String
+            password: String
+        }
+        """
         raise NotImplementedError("You must implement login")
 
     def get_user_info(self):
         raise NotImplementedError("You must implement get_user_info")
 
-    """ list
-        "spoed": Boolean,
-        "datumMelding": "YYYY-MM-DDTHH:MM:SS",
-        "afdeling": {
-            "id": String,
-            "omschrijving": String,
-        },
-        "id": Number,
-        "status": String,
-        "onderwerp": {
-            "id": String,
-            "omschrijving": String
-        },
-        "omschrijving": String,
-        "locatie": {
-            "adres": {
-                "straatNummer": String,
-                "straatNaam": String,
-                "huisnummer": String,
-            },
-            "x": Number,
-            "y": Number,
-        },
-    """
     def get_list(self):
-        raise NotImplementedError("You must implement get_list")
-
-    """ detail
-        "success": Boolean,
-        "result": {
-            "id": Number,
-            "status": String,
-            "meldingType": String,
-            "datumMelding": "YYYY-MM-DDTHH:MM:SS",
-            "onderwerp": {
-                "id": String,
-                "omschrijving": String
-            },
-            "afdeling": {
-                "id": String,
-                "omschrijving": String,
-            },
-            "omschrijving": String,
-            "behandelaar": String,
-            "melder": {
-                "herkomst": String,
-                "naam": String,
-                "telefoon": String,
-                "informeren": String["Ja", "Nee"],
-            },
-            "locatie": {
-                "buurtNummer": String,
-                "subbuurt": String,
-                "adres": {
-                    "straatNummer": String,
-                    "straatNaam": String,
-                    "huisnummer": String,
-                },
-                "plaatsbepaling": String,
-                "x": Number,
-                "y": Number,
-            },
-            "fotos": [],
-            "magDoorverwijzen": Boolean,
-            "magAfhandelen": Boolean,
-        }
-    """
-    def get_detail(self):
-        raise NotImplementedError("You must implement get_detail")
-
-    """ mutatieregels
-        "success": Boolean,
-        "result": [
+        """ 
+        get list [
             {
-                "datum": String,
-                "gebruiker": String,
-                "opmerking": String,
-                "details": [
-                    {
-                        "key": String,
-                        "value": String
-                    }
-                ],
+                "spoed": Boolean,
+                "datumMelding": "YYYY-MM-DDTHH:MM:SS",
+                "afdeling": {
+                    "id": String,
+                    "omschrijving": String,
+                },
+                "id": Number,
+                "status": String,
+                "onderwerp": {
+                    "id": String,
+                    "omschrijving": String
+                },
+                "omschrijving": String,
+                "locatie": {
+                    "adres": {
+                        "straatNummer": String,
+                        "straatNaam": String,
+                        "huisnummer": String,
+                    },
+                    "x": Number,
+                    "y": Number,
+                }
             }
         ]
-    """
+        """
+        raise NotImplementedError("You must implement get_list")
+
+    def get_detail(self):
+        """
+        get detail {
+            "success": Boolean,
+            "result": {
+                "id": Number,
+                "status": String,
+                "meldingType": String,
+                "datumMelding": "YYYY-MM-DDTHH:MM:SS",
+                "onderwerp": {
+                    "id": String,
+                    "omschrijving": String
+                },
+                "afdeling": {
+                    "id": String,
+                    "omschrijving": String,
+                },
+                "omschrijving": String,
+                "behandelaar": String,
+                "melder": {
+                    "herkomst": String,
+                    "naam": String,
+                    "telefoon": String,
+                    "informeren": String["Ja", "Nee"],
+                },
+                "locatie": {
+                    "buurtNummer": String,
+                    "subbuurt": String,
+                    "adres": {
+                        "straatNummer": String,
+                        "straatNaam": String,
+                        "huisnummer": String,
+                    },
+                    "plaatsbepaling": String,
+                    "x": Number,
+                    "y": Number,
+                },
+                "fotos": [],
+                "magDoorverwijzen": Boolean,
+                "magAfhandelen": Boolean,
+            }
+        }
+        """
+        raise NotImplementedError("You must implement get_detail")
+
     def get_mutatieregels(self):
+        """
+        get mutatieregels {
+            "success": Boolean,
+            "result": [
+                {
+                    "datum": String,
+                    "gebruiker": String,
+                    "opmerking": String,
+                    "details": [
+                        {
+                            "key": String,
+                            "value": String
+                        }
+                    ],
+                }
+            ]
+        }
+        """
         raise NotImplementedError("You must implement get_mutatieregels")
 
     def get_foto(self):
         raise NotImplementedError("You must implement get_foto")
 
-    """ wijken
-        "success": Boolean,
-        "result": [
-            {
-                "code": String,
-                "omschrijving": String,
-                "buurten": [
-                    {   
-                        "code": String,
-                        "omschrijving": String
-                    }
-                ],
-            }
-        ],
-    """
     def get_wijken(self):
+        """ 
+        get wijken {
+            "success": Boolean,
+            "result": [
+                {
+                    "code": String,
+                    "omschrijving": String,
+                    "buurten": [
+                        {   
+                            "code": String,
+                            "omschrijving": String
+                        }
+                    ],
+                }
+            ]
+        }
+        """
         raise NotImplementedError("You must implement get_foto")
 
-    """ onderwerpgroepen
-        "success": Boolean,
-        "result": [
-            {
-                "code": String,
-                "omschrijving": String,
-                "onderwerpen": [
-                    {
-                        "code": String,
-                        "omschrijving": String,
-                        "magDoorverwijzen": Boolean,
-                    }
-                ],
-            }
-        ]
-    """
     def get_onderwerpgroepen(self):
+        """ 
+        get onderwerpgroepen {
+            "success": Boolean,
+            "result": [
+                {
+                    "code": String,
+                    "omschrijving": String,
+                    "onderwerpen": [
+                        {
+                            "code": String,
+                            "omschrijving": String,
+                            "magDoorverwijzen": Boolean,
+                        }
+                    ],
+                }
+            ]
+        }
+        """
         raise NotImplementedError("You must implement get_onderwerpgroepen")
 
-    """" afdelingen
-        success": Boolean,
-        "result": [
-            {
-                "code": String,
-                "omschrijving": String
-            }
-        ]
-    """
     def get_afdelingen(self):
+        """" 
+        get afdelingen {
+            success": Boolean,
+            "result": [
+                {
+                    "code": String,
+                    "omschrijving": String
+                }
+            ]
+        }
+        """
         raise NotImplementedError("You must implement get_afdelingen")
 
     def get_afdeling_relaties(self):
@@ -165,4 +185,15 @@ class DefaultIncidentAPIService(APIService):
         raise NotImplementedError("You must implement melding_aanmaken")
 
     def afhandelen(self):
+        """
+        post afhandelen {
+            "meldingId": Number,
+            "behandelaar": String,
+            "meldingType": String ("N", "O"),
+            "redenAfhandelenNiet": String,
+            "afhandelOpmerking": String,
+            "straat": String,
+            "huisnummer": String,
+        }
+        """
         raise NotImplementedError("You must implement afhandelen")
